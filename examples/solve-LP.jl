63b0acf1cb747a7facbc17f92b0cab7e40551176
Akshay Sharma <contact@imakshay.com>
2020-06-09 23:53:17 +0530

    access dual variable values via  MOI directly

--- examples/solve-LP.jl
+++ examples/solve-LP.jl
@@ -1,7 +1,6 @@
 using Random
 using GLPK
 using MathOptInterface
-using Dualization
 
 const MOI  = MathOptInterface
 const MOIU = MathOptInterface.Utilities;
@@ -49,35 +48,18 @@ x̄ = MOI.get(model, MOI.VariablePrimal(), x);  # solution
 
 @assert abs(c'x̄ - c'x̂) <= 1e-8   # sanity check
 
-joint_object    = dualize(model)
-dual_model_like = joint_object.dual_model # this is MOI.ModelLike, not an MOI.AbstractOptimizer; can't call optimizer on it
-primal_dual_map = joint_object.primal_dual_map;
 
-# copy the dual model objective, constraints, and variables to an optimizer
-dual_model = GLPK.Optimizer()
-MOI.copy_to(dual_model, dual_model_like)
-
-# solve dual
-MOI.optimize!(dual_model);
-
-# NOTE: You can obtain components of the dual model individually by -
-# dual_objective = dual_model_like.objective  # b'y
-# dual_variable_indices = [primal_dual_map.primal_con_dual_var[x][1] for x in constraint_indices]
-# dual_constraint_indices = [primal_dual_map.primal_var_dual_con[i] for i in x];
-
-# ŷ = MOI.get(dm, MOI.VariablePrimal(), dual_variable_indices)
-
-# check if strong duality holds
-@assert abs(MOI.get(model, MOI.ObjectiveValue()) - MOI.get(dual_model, MOI.ObjectiveValue())) <= 1e-8
+##
+## Checking KKT conditions
+##
 
 is_less_than(set::S) where {S<:MOI.AbstractSet} = false
 is_less_than(set::MOI.LessThan{T}) where T = true
 
-map = primal_dual_map.primal_con_dual_var
-for con_index in keys(map)
+for con_index in constraint_indices
     con_value = MOI.get(model, MOI.ConstraintPrimal(), con_index)
     set = MOI.get(model, MOI.ConstraintSet(), con_index)
-    μ         = MOI.get(dual_model, MOI.VariablePrimal(), map[con_index][1])
+    μ = MOI.get(model, MOI.ConstraintDual(), con_index)
     
     if is_less_than(set)
         # μ[i]*(Ax - b)[i] = 0
--- examples/solve-QP.jl
+++ examples/solve-QP.jl
@@ -1,6 +1,5 @@
 using Random
 using MathOptInterface
-using Dualization
 using OSQP
 
 const MOI = MathOptInterface
@@ -33,15 +32,17 @@ MOI.set(model, MOI.ObjectiveFunction{MOI.ScalarQuadraticFunction{Float64}}(), ob
 MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
 
 # maintain constrain to index map - will be useful later
-constraint_map = Dict()
+constraint_indices = []
 
 # add constraints
 for i in 1:m
-    ci = MOI.add_constraint(
-        model,
-        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(G[i,:], x), 0.),MOI.LessThan(h[i])
+    push!(
+        constraint_indices,
+        MOI.add_constraint(
+            model,
+            MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(G[i,:], x), 0.),MOI.LessThan(h[i])
+        )
     )
-    constraint_map[ci] = i
 end
 
 MOI.optimize!(model)
@@ -56,35 +57,14 @@ x̄ = MOI.get(model, MOI.VariablePrimal(), x);
 # NOTE: can't use Ipopt
 # Ipopt.Optimizer doesn't supports accessing MOI.ObjectiveFunctionType
 
-joint_object    = dualize(model)
-dual_model_like = joint_object.dual_model # this is MOI.ModelLike, not an MOI.AbstractOptimizer; can't call optimizer on it
-primal_dual_map = joint_object.primal_dual_map;
-
-# copy the dual model objective, constraints, and variables to an optimizer
-dual_model = MOI.instantiate(OSQP.Optimizer, with_bridge_type=Float64)
-MOI.copy_to(dual_model, dual_model_like)
-
-# solve dual
-MOI.optimize!(dual_model);
-
-# check if strong duality holds
-@assert abs(MOI.get(model, MOI.ObjectiveValue()) - MOI.get(dual_model, MOI.ObjectiveValue())) <= 1e-1
-
-
 #
 # Verifying KKT Conditions
 #
 
-map = primal_dual_map.primal_con_dual_var;
-
-
 # complimentary slackness  + dual feasibility
-for con_index in keys(map)
-    # NOTE: OSQP.Optimizer doesn't allows access to MOI.ConstraintPrimal
-    #       That's why I defined a custom map 
-    
-    i   = constraint_map[con_index]
-    μ_i = MOI.get(dual_model, MOI.VariablePrimal(), map[con_index][1])
+for i in 1:size(constraint_indices)[1]
+    con_index = constraint_indices[i]
+    μ_i = MOI.get(model, MOI.ConstraintDual(), con_index)
     
     # μ[i] * (G * x - h)[i] = 0
     @assert abs(μ_i * (G[i,:]' * x̄ - h[i])) < 1e-2
@@ -98,12 +78,9 @@ end
 for j in 1:n
     G_mu_sum = 0
 
-    for con_index in keys(map)
-        # NOTE: OSQP.Optimizer doesn't allows access to MOI.ConstraintPrimal
-        #       That's why I defined a custom map 
-        
-        i = constraint_map[con_index]
-        μ_i = MOI.get(dual_model, MOI.VariablePrimal(), map[con_index][1])
+    for i in 1:size(constraint_indices)[1]
+        con_index = constraint_indices[i]
+        μ_i = MOI.get(model, MOI.ConstraintDual(), con_index)
 
         G_mu_sum += μ_i * G[i,j]
     end
