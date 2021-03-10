de66f1cb892768af8d2dd18d9956ffcacefece0e
Akshay <contact@imakshay.com>
2021-03-10 18:53:32 +0530

    add more test

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -723,6 +723,102 @@ end
     @test grads[6] ≈ [1/3]
 end
 
+
+@testset "Differentiating LP with SAF, SV with LE, GE constraints" begin
+    """
+        max 2x + 3y + 4z
+        s.t. 3x+2y+z <= 10
+            2x+5y+3z <= 15
+            x ≤ 3
+            0 ≤ y ≤ 2
+            6 ≥ z ≥ -1
+            x,y,z >= 0
+        variant of previous test with same solution
+    """
+    model = diff_optimizer(GLPK.Optimizer)
+    MOI.set(model, MOI.Silent(), true)
+    v = MOI.add_variables(model, 3)
+
+    # define objective
+    objective_function = MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([-2.0, -3.0, -4.0], v), 0.0)
+    MOI.set(model, MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), objective_function)
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+
+    # set constraints
+    MOI.add_constraint(
+        model,
+        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([3.0, 2.0, 1.0], v), 0.),
+        MOI.LessThan(10.0),
+    )
+    MOI.add_constraint(
+        model,
+        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([2.0, 5.0, 3.0], v), 0.),
+        MOI.LessThan(15.0),
+    )
+    #      -x ≤ 0
+    MOI.add_constraint(
+        model,
+        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([-1.0, 0.0, 0.0], v), 0.),
+        MOI.LessThan(0.0),
+    )
+    #      -y ≤ 0
+    MOI.add_constraint(
+        model,
+        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([0.0, -1.0, 0.0], v), 0.),
+        MOI.LessThan(0.0),
+    )
+    #      0 ≤ z
+    MOI.add_constraint(
+        model,
+        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([0.0, 0.0, 1.0], v), 0.),
+        MOI.GreaterThan(0.0),
+    )
+    #      x ≤ 3
+    MOI.add_constraint(
+        model,
+        MOI.SingleVariable(v[1]),
+        MOI.LessThan(3.0),
+    )
+    #      y ≤ 2
+    MOI.add_constraint(
+        model,
+        MOI.SingleVariable(v[2]),
+        MOI.LessThan(2.0),
+    )
+    #      6 ≥ z
+    MOI.add_constraint(
+        model,
+        MOI.SingleVariable(v[3]),
+        MOI.LessThan(6.0),
+    )
+    #      z ≥ -1
+    MOI.add_constraint(
+        model,
+        MOI.SingleVariable(v[3]),
+        MOI.GreaterThan(-1.0),
+    )
+
+    MOI.optimize!(model)
+
+    # obtain gradients
+    grads = backward(model, ["Q", "q", "G", "h"], ones(3))  # using dl_dz=[1,1,1]
+
+    @test grads[1] ≈ zeros(3,3) atol=ATOL rtol=RTOL
+    @test grads[2] ≈ zeros(3) atol=ATOL rtol=RTOL
+    @test grads[3] ≈ [0.0 0.0 0.0;
+                      0.0 0.0 -5/3;
+                      0.0 0.0 5/3;
+                      0.0 0.0 -10/3;
+                      0.0 0.0 0.0
+                      0.0 0.0 0.0
+                      0.0 0.0 0.0
+                      0.0 0.0 0.0
+                      0.0 0.0 0.0
+                      ]   atol=ATOL rtol=RTOL
+    @test grads[4] ≈ [0.0, 1/3, -1/3, 2/3, 0.0, 0.0, 0.0, 0.0, 0.0]   atol=ATOL rtol=RTOL
+end
+
+
 @testset "Differentiating simple SOCP" begin
     # referred from _soc2test, https://github.com/jump-dev/MathOptInterface.jl/blob/master/src/Test/contconic.jl#L1355
     # find equivalent diffcp python program here: https://github.com/AKS1996/jump-gsoc-2020/blob/master/diffcp_socp_1_py.ipynb
