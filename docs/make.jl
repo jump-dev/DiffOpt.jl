2cce3c11811aa71d0e20ab9d2e145a5aacce4534
Akshay <static.akshay@gmail.com>
2021-07-20 14:18:54 +0530

    example 1

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,43 @@
-using Documenter, DiffOpt
+using Documenter, DiffOpt, Literate, Test
+
+const _EXAMPLE_DIR = joinpath(@__DIR__, "src", "examples")
+
+"""
+    _include_sandbox(filename)
+Include the `filename` in a temporary module that acts as a sandbox. (Ensuring
+no constants or functions leak into other files.)
+"""
+function _include_sandbox(filename)
+    mod = @eval module $(gensym()) end
+    return Base.include(mod, filename)
+end
+
+function _file_list(full_dir, relative_dir, extension)
+    return map(
+        file -> joinpath(relative_dir, file),
+        filter(file -> endswith(file, extension), sort(readdir(full_dir))),
+    )
+end
+
+function literate_directory(dir)
+    rm.(_file_list(dir, dir, ".md"))
+    for filename in _file_list(dir, dir, ".jl")
+        # `include` the file to test it before `#src` lines are removed. It is
+        # in a testset to isolate local variables between files.
+        @testset "$(filename)" begin
+            _include_sandbox(filename)
+        end
+        Literate.markdown(
+            filename,
+            dir;
+            documenter = true,
+        )
+    end
+    return
+end
+
+literate_directory(_EXAMPLE_DIR)
+
 
 makedocs(;
     modules=[DiffOpt],
@@ -6,7 +45,7 @@ makedocs(;
     clean = true,
     format=Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
-        mathengine = Documenter.MathJax(),
+        mathengine = Documenter.MathJax2(),
     ),
     pages=[
         "Home" => "index.md",
@@ -19,8 +58,10 @@ makedocs(;
             "Solving a QP" => "solve-QP.md",
             "Solving conic with PSD and SOC constraints" => "solve-conic-1.md",
             "Differentiating a simple QP by hand" => "matrix-inversion-manual.md",
-            "Sensitivity Analysis SVM" => "sensitivity-analysis-svm.md",
             "ChainRules integration" => "chainrules_unit.md",
+        ],
+        "Tutorials" => [
+            joinpath("examples", f) for f in readdir(_EXAMPLE_DIR) if endswith(f, ".md")
         ],
     ],
     strict = true,  # See https://github.com/JuliaOpt/JuMP.jl/issues/1576
