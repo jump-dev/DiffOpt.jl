3e86427690303fc77074fb36fb91be2dc123c3aa
joaquim <joaquimgarcia@psr-inc.com>
2021-12-02 04:29:30 -0300

    fix and simplify svm example

--- docs/src/examples/sensitivity-analysis-svm.jl
+++ docs/src/examples/sensitivity-analysis-svm.jl
@@ -11,9 +11,9 @@
 # ```math
 # \begin{split}
 # \begin{array} {ll}
-# \mbox{minimize} & \sum_{i=1}^{N} \xi_{i} \\
+# \mbox{minimize} & \lambda||w||^2 \sum_{i=1}^{N} \xi_{i} \\
 # \mbox{s.t.} & \xi_{i} \ge 0 \quad i=1..N  \\
-#             & y_{i} (w^T X_{i} + b) \ge 1 - \xi[i]\\
+#             & y_{i} (w^T X_{i} + b) \ge 1 - \xi_{i}\\
 # \end{array}
 # \end{split}
 # ```
@@ -26,7 +26,7 @@
 # Import the libraries.
 
 
-using SCS, DiffOpt, LinearAlgebra, JuMP
+using Ipopt, DiffOpt, LinearAlgebra, JuMP
 import Random, Plots
 
 
@@ -41,12 +41,13 @@ y = append!(ones(N ÷ 2), -ones(N ÷ 2));
 
 # Let's initialize a special model that can understand sensitivities
 
-model = Model(() -> diff_optimizer(SCS.Optimizer))
+# model = Model(() -> diff_optimizer(SCS.Optimizer))
+model = Model(() -> diff_optimizer(Ipopt.Optimizer))
 MOI.set(model, MOI.Silent(), true)
 
 # Add the variables
 
-@variable(model, l[1:N])
+@variable(model, ξ[1:N])
 @variable(model, w[1:D])
 @variable(model, b);
 
@@ -54,12 +55,13 @@ MOI.set(model, MOI.Silent(), true)
 
 @constraint(
     model,
-    1.0 * l ∈ MOI.Nonnegatives(N),
-)
+    [i in 1:N],
+    ξ[i] >=0
+);
 @constraint(
     model,
-    cons,
-    y .* (X * w .+ b) + l .- 1 ∈ MOI.Nonnegatives(N),
+    cons[i in 1:N],
+    y[i] * (dot(X[i, :], w) + b) + ξ[i] - 1 >= 0
 );
 
 
@@ -68,7 +70,7 @@ MOI.set(model, MOI.Silent(), true)
 @objective(
     model,
     Min,
-    sum(l),
+    0.05*dot(w,w) + sum(ξ),
 )
 
 optimize!(model) # solve
@@ -77,7 +79,9 @@ optimize!(model) # solve
 # We can visualize the separating hyperplane.
 
 loss = objective_value(model)
+
 wv = value.(w)
+
 bv = value(b)
 
 svm_x = [0.0, 3.0]
@@ -94,109 +98,26 @@ Plots.plot!(p, svm_x, svm_y, label = "loss = $(round(loss, digits=2))", width=3)
 # - How does a change in labels of the data points (`y=1` to `y=-1`, and vice versa) affect the position of the hyperplane? This is achieved by finding the gradient of `w`, `b` with respect to `y[i]`, the classification label of the ith data point.
 # - How does a change in coordinates of the data points, `X`, affects the position of the hyperplane? This is achieved by finding gradient of `w`, `b` with respect to `X[i]`, 2D coordinates of the data points.
 
-# Note that finding the optimal SVM can be modelled as a conic optimization problem:
-
-# ```math
-# \begin{align*}
-# & \min_{x \in \mathbb{R}^n} & c^T x \\
-# & \text{s.t.}               & A x + s = b  \\
-# &                           & b \in \mathbb{R}^m  \\
-# &                           & s \in \mathcal{K}
-# \end{align*}
-# ```
-
-# where
-# ```math
-# \begin{align*}
-# c &= [l_1 - 1, l_2 -1, ... l_N -1, 0, 0, ... 0 \text{(D+1 times)}] \\\\
-
-# A &=
-# \begin{bmatrix}
-#  -l_1 &    0 & ... &    0 &            0 & ... & 0 & 0  \\
-#     0 & -l_2 & ... &    0 &            0 & ... & 0 & 0  \\
-#     : &    : & ... &    : &            0 & ... & 0 & 0  \\
-#     0 &    0 & ... & -l_N &            0 & ... & 0 & 0  \\
-#     0 &    0 & ... &    0 & -y_1 X_{1,1} & ... & -y_1 X_{1,N} & -y_1  \\
-#     0 &    0 & ... &    0 & -y_2 X_{2,1} & ... & -y_1 X_{2,N} & -y_2  \\
-#     : &    : & ... &    : &           :  & ... &          :   & :   \\
-#     0 &    0 & ... &    0 & -y_N X_{N,1} & ... & -y_N X_{N,N} & -y_N  \\
-# \end{bmatrix} \\\\
-
-# b &= [0, 0, ... 0 \text{(N times)}, l_1 - 1, l_2 -1, ... l_N -1] \\\\
-
-# \mathcal{K} &= \text{Set of Nonnegative cones}
-# \end{align*}
-# ```
-
-
 # ## Experiment 1: Gradient of hyperplane wrt the data point labels
 
 # Construct perturbations in data point labels `y` without changing the data point coordinates `X`.
 
-
-∇ = Float64[]
+∇ = zeros(N)
 dy = zeros(N);
 
 # begin differentiating
-for Xi in 1:N
-    dy[Xi] = 1.0  # set
+for i in 1:N
+    dy[i] = 1.0 # set
 
-    MOI.set(
-        model,
-        DiffOpt.ForwardInConstraint(),
-        cons,
-        MOI.Utilities.vectorize(dy .* index(b)),
-    )
-
-    DiffOpt.forward(model)
-
-    dw = MOI.get.(
-        model,
-        DiffOpt.ForwardOutVariablePrimal(),
-        w,
-    )
-    db = MOI.get(
-        model,
-        DiffOpt.ForwardOutVariablePrimal(),
-        b,
-    )
-    push!(∇, norm(dw) + norm(db))
-
-    dy[Xi] = 0.0  # reset the change made above
-end
-
-normalize!(∇);
-
-
-# Visualize point sensitivities with respect to separating hyperplane. Note that the gradients are normalized.
-
-p2 = Plots.scatter(
-    X[:,1], X[:,2],
-    color = [yi > 0 ? :red : :blue for yi in y], label = "",
-    markersize = ∇ * 20,
-)
-Plots.yaxis!(p2, (-2, 4.5))
-Plots.plot!(p2, svm_x, svm_y, label = "loss = $(round(loss, digits=2))", width=3)
-
-
-# ## Experiment 2: Gradient of hyperplane wrt the data point coordinates
-
-# Similar to previous example, construct perturbations in data points coordinates `X`.
-
-∇ = Float64[]
-dX = zeros(N, D);
-
-# begin differentiating
-for Xi in 1:N
-    dX[Xi, :] = ones(D)  # set
-
-    for i in 1:D
+    for j in 1:N
         MOI.set(
             model,
             DiffOpt.ForwardInConstraint(),
-            cons,
-            MOI.Utilities.vectorize(dX[:,i] .* index(w[i])),
+            cons[j],
+            dy[j] * (dot(X[j,:], index.(w)) + index(b)),
         )
+        # analogous to varying theta in the expression:
+        # (y_{i} + theta) (w^T X_{i} + b) \ge 1 - \xi_{i}
     end
 
     DiffOpt.forward(model)
@@ -211,9 +132,64 @@ for Xi in 1:N
         DiffOpt.ForwardOutVariablePrimal(),
         b,
     )
-    push!(∇, norm(dw) + norm(db))
 
-    dX[Xi, :] = zeros(D)  # reset the change made at the beginning of the loop
+    ∇[i] = norm(dw) + norm(db)
+
+    dy[i] = 0.0  # reset the change made above
+end
+
+normalize!(∇);
+
+
+# Visualize point sensitivities with respect to separating hyperplane. Note that the gradients are normalized.
+
+p2 = Plots.scatter(
+    X[:,1], X[:,2],
+    color = [yi > 0 ? :red : :blue for yi in y], label = "",
+    markersize = 20 * max.(∇, 0.2 * maximum(∇)),
+)
+Plots.yaxis!(p2, (-2, 4.5))
+Plots.plot!(p2, svm_x, svm_y, label = "loss = $(round(loss, digits=2))", width=3)
+
+
+# ## Experiment 2: Gradient of hyperplane wrt the data point coordinates
+
+# Similar to previous example, construct perturbations in data points coordinates `X`.
+
+∇ = zeros(N)
+dX = zeros(N, D);
+
+# begin differentiating
+for i in 1:N
+    dX[i, :] = ones(D)  # set
+
+    for j in 1:N
+        MOI.set(
+            model,
+            DiffOpt.ForwardInConstraint(),
+            cons[j],
+            y[j] * dot(dX[j,:], index.(w)),
+        )
+        # analogous to varying theta in the expression:
+        # y_{i} (w^T (X_{i} + theta) + b) \ge 1 - \xi_{i}
+    end
+
+    DiffOpt.forward(model)
+
+    dw = MOI.get.(
+        model,
+        DiffOpt.ForwardOutVariablePrimal(),
+        w,
+    )
+    db = MOI.get(
+        model,
+        DiffOpt.ForwardOutVariablePrimal(),
+        b,
+    )
+
+    ∇[i] = norm(dw) + norm(db)
+
+    dX[i, :] = zeros(D)  # reset the change made at the beginning of the loop
 end
 
 normalize!(∇);
@@ -222,8 +198,8 @@ normalize!(∇);
 
 p3 = Plots.scatter(
     X[:,1], X[:,2],
-    color = [yi > 0 ? :red : :blue for yi in y], label = "",
-    markersize = ∇ * 20,
+    color = [yi > 0 ? :red : :blue for yi in y], label = "",#["$i" for i in 1:N],
+    markersize = 20 * max.(∇, 0.2 * maximum(∇)),
 )
 Plots.yaxis!(p3, (-2, 4.5))
 Plots.plot!(p3, svm_x, svm_y, label = "loss = $(round(loss, digits=2))", width=3)
