ff9cec9d61ce1230bf136da4a166c40d4eab0989
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-04-11 18:50:49 +0200

    more details

--- docs/src/examples/polyhedral_project.jl
+++ docs/src/examples/polyhedral_project.jl
@@ -1,12 +1,19 @@
-# # Custom ReLU layer
+# # Polyhedral QP layer
 
-#md # [![](https://img.shields.io/badge/show-github-579ACA.svg)](@__REPO_ROOT_URL__/docs/src/examples/custom-relu.jl)
+#md # [![](https://img.shields.io/badge/show-github-579ACA.svg)](@__REPO_ROOT_URL__/docs/src/examples/polyhedral_project.jl)
 
-# We demonstrate how DiffOpt can be used to generate a simple neural network
-# unit - the ReLU layer. A neural network is created using Flux.jl and
-# trained on the MNIST dataset.
+# We use DiffOpt to define a custom network layer which, given an input matrix `y`,
+# computes its projection onto a polytope defined by a fixed number of inequalities:
+# `a_i^T x ≥ b_i`.
+# A neural network is created using Flux.jl and trained on the MNIST dataset,
+# integrating this quadratic optimization layer.
+#
+# The QP is solved in the forward pass, and its DiffOpt derivative is used in the backward pass expressed with `ChainRulesCore.rrule`.
 
-# This tutorial uses the following packages
+# This example is similar to the custom ReLU layer, except that the layer is parameterized
+# by the hyperplanes `(w,b)` and not a simple stateless function.
+# This also means that `ChainRulesCore.rrule` must return the derivatives of the output with respect to the
+# layer parameters to allow for backpropagation.
 
 using JuMP
 import DiffOpt
@@ -17,33 +24,46 @@ import MLDatasets
 import Statistics
 using Base.Iterators: repeated
 using LinearAlgebra
+using Random
 
-# ## The ReLU and its derivative
+Random.seed!(42)
 
-struct MaxOfN{N}
+# ## The Polytope representation and its derivative
+
+struct Polytope{N}
     w::NTuple{N, Matrix{Float64}}
     b::Vector{Float64}
 end
 
-MaxOfN(w::NTuple{N, Matrix{Float64}}) where {N} = MaxOfN{N}(w, randn(N))
+Polytope(w::NTuple{N}) where {N} = Polytope{N}(w, randn(N))
 
-Flux.@functor MaxOfN
-
-function (maxofn::MaxOfN)(y::AbstractMatrix; model = direct_model(DiffOpt.diff_optimizer(Ipopt.Optimizer)))
+# We define a "call" operation on the polytope, making it a so-called functor.
+# Calling the polytope with a matrix `y` operates an Euclidean projection of this matrix onto the polytope.
+function (polytope::Polytope)(y::AbstractMatrix; model = direct_model(DiffOpt.diff_optimizer(Ipopt.Optimizer)))
     N, M = size(y)
     empty!(model)
-    # set_silent(model)
+    set_silent(model)
     @variable(model, x[1:N, 1:M])
-    @constraint(model, greater_than_cons[idx in 1:length(maxofn.w)], dot(maxofn.w[idx], x) ≥ maxofn.b[idx])
+    @constraint(model, greater_than_cons[idx in 1:length(polytope.w)], dot(polytope.w[idx], x) ≥ polytope.b[idx])
     @objective(model, Min, dot(x - y, x - y))
     optimize!(model)
     return JuMP.value.(x)
 end
 
-# Define the backward differentiation rule, for the function we defined above.
-function ChainRulesCore.rrule(maxofn::MaxOfN, y::AbstractMatrix)
+# The `@functor` macro from Flux implements auxiliary functions for collecting the parameters of
+# our custom layer and operating backpropagation.
+Flux.@functor Polytope
+
+# Define the reverse differentiation rule, for the function we defined above.
+# Flux uses ChainRules primitives to implement reverse-mode differentiation of the whole network.
+# To learn the current layer (the polytope the layer contains),
+# the gradient is computed with respect to the `Polytope` fields in a ChainRulesCore.Tangent type
+# which is used to represent derivatives with respect to structs.
+# For more details about backpropagation, visit [Introduction, ChainRulesCore.jl](https://juliadiff.org/ChainRulesCore.jl/dev/).
+
+function ChainRulesCore.rrule(polytope::Polytope, y::AbstractMatrix)
     model = direct_model(DiffOpt.diff_optimizer(Ipopt.Optimizer))
-    xv = maxofn(y; model = model)
+    xv = polytope(y; model = model)
     function pullback_matrix_projection(dl_dx)
         dl_dx = ChainRulesCore.unthunk(dl_dx)
         ##  `dl_dy` is the derivative of `l` wrt `y`
@@ -51,8 +71,8 @@ function ChainRulesCore.rrule(maxofn::MaxOfN, y::AbstractMatrix)
         ## grad wrt input parameters
         dl_dy = zeros(size(dl_dx))
         ## grad wrt layer parameters
-        dl_dw = zero.(maxofn.w)
-        dl_db = zero(maxofn.b)
+        dl_dw = zero.(polytope.w)
+        dl_db = zero(polytope.b)
         ## set sensitivities
         MOI.set.(model, DiffOpt.BackwardInVariablePrimal(), x, dl_dx)
         ## compute grad
@@ -66,15 +86,14 @@ function ChainRulesCore.rrule(maxofn::MaxOfN, y::AbstractMatrix)
             dl_db[idx] = -JuMP.constant(cons_expr)
             dl_dw[idx] .= JuMP.coefficient.(cons_expr, x)
         end
-        dself = ChainRulesCore.Tangent{typeof(maxofn)}(; w = dl_dw, b = dl_db)
+        dself = ChainRulesCore.Tangent{typeof(polytope)}(; w = dl_dw, b = dl_db)
         return (dself, dl_dy)
     end
     return xv, pullback_matrix_projection
 end
 
-# For more details about backpropagation, visit [Introduction, ChainRulesCore.jl](https://juliadiff.org/ChainRulesCore.jl/dev/).
-# ## prepare data
-N = 1000
+# ## Prepare data
+N = 500
 imgs = MLDatasets.MNIST.traintensor(1:N)
 labels = MLDatasets.MNIST.trainlabels(1:N);
 
@@ -88,14 +107,12 @@ test_Y = Flux.onehotbatch(MLDatasets.MNIST.testlabels(1:N), 0:9);
 
 # ## Define the Network
 
-# Network structure
-
-inner = 100
+inner = 20
 
 m = Flux.Chain(
-    Flux.Dense(784, inner), #784 being image linear dimension (28 x 28)
-    MaxOfN((randn(inner,1000), randn(inner,1000), randn(inner,1000))),
-    Flux.Dense(inner, 10), # 10 beinf the number of outcomes (0 to 9)
+    Flux.Dense(784, inner), ## 784 being image linear dimension (28 x 28)
+    Polytope((randn(inner, N), randn(inner, N), randn(inner, N))),
+    Flux.Dense(inner, 10), ## 10 being the number of outcomes (0 to 9)
     Flux.softmax,
 )
 
@@ -103,8 +120,7 @@ m = Flux.Chain(
 # The original data is repeated `epochs` times because `Flux.train!` only
 # loops through the data set once
 
-epochs = 50 # ~1 minute (i7 8th gen with 16gb RAM)
-## epochs = 100 # leads to 77.8% in about 2 minutes
+epochs = 50
 
 dataset = repeated((train_X, train_Y), epochs);
 
@@ -117,7 +133,7 @@ evalcb = () -> @show(custom_loss(train_X, train_Y))
 
 # Train to optimize network parameters
 
-@time Flux.train!(custom_loss, Flux.params(m), dataset, opt, cb = Flux.throttle(evalcb, 5));
+Flux.train!(custom_loss, Flux.params(m), dataset, opt, cb = Flux.throttle(evalcb, 5));
 
 # Although our custom implementation takes time, it is able to reach similar
 # accuracy as the usual ReLU function implementation.
