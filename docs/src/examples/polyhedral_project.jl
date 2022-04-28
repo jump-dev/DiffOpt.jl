34d422e54de7cfdfcd0a81bae873a5638ebd7419
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-04-28 09:35:00 +0200

    Update docs/src/examples/polyhedral_project.jl

--- docs/src/examples/polyhedral_project.jl
+++ docs/src/examples/polyhedral_project.jl
@@ -67,7 +67,7 @@ Flux.@functor Polytope
 # which is used to represent derivatives with respect to structs.
 # For more details about backpropagation, visit [Introduction, ChainRulesCore.jl](https://juliadiff.org/ChainRulesCore.jl/dev/).
 
-function ChainRulesCore.rrule(polytope::Polytope{N}, y::AbstractMatrix) where N
+function ChainRulesCore.rrule(polytope::Polytope{N}, y::AbstractMatrix) where {N}
     model = direct_model(DiffOpt.diff_optimizer(Ipopt.Optimizer))
     xv = polytope(y; model = model)
     function pullback_matrix_projection(dl_dx)
