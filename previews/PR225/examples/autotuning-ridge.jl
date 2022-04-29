92f1d1d3e30001ef19b151cafed01ad2976986f3
Documenter.jl <documenter@juliadocs.github.io>
2022-04-29 15:28:04 +0000

    build based on 4877725

---
+++ previews/PR225/examples/autotuning-ridge.jl
@@ -0,0 +1,208 @@
+# # Auto-tuning Hyperparameters
+
+#md # [![](https://img.shields.io/badge/show-github-579ACA.svg)](@__REPO_ROOT_URL__/docs/src/examples/autotuning-ridge.jl)
+
+# This example shows how to learn a hyperparameter in Ridge Regression using a gradient descent routine.
+# Let the regularized regression problem be formulated as:
+
+# ```math
+# \begin{equation}
+# \min_{w} \quad \frac{1}{2nd} \sum_{i=1}^{n} (w^T x_{i} - y_i)^2 + \frac{\alpha}{2d} \| w \|_2^2
+# \end{equation}
+# ```
+
+# where 
+# - `x`, `y` are the data points
+# - `w` are the learned weights
+# - `α` is the hyperparameter acting on regularization.
+
+# The main optimization model will be formulated with JuMP.
+# Using the gradient of the optimal weights with respect to the regularization parameters
+# computed with DiffOpt, we can perform a gradient descent on top of the inner model
+# to minimize the test loss.
+
+# This tutorial uses the following packages
+
+using JuMP     # The mathematical programming modelling language
+import DiffOpt # JuMP extension for differentiable optimization
+import Ipopt    # Optimization solver that handles quadratic programs
+import Plots   # Graphing tool
+import LinearAlgebra: norm, dot
+import Random
+
+# ## Generating a noisy regression dataset
+
+Random.seed!(42)
+
+N = 100
+D = 20
+noise = 5
+
+w_real = 10 * randn(D)
+X = 10 * randn(N, D)
+y = X * w_real + noise * randn(N)
+l = N ÷ 2  # test train split
+
+X_train = X[1:l, :]
+X_test  = X[l+1:N, :]
+y_train = y[1:l]
+y_test  = y[l+1:N];
+
+# ## Defining the regression problem
+
+# We implement the regularized regression problem as a function taking the problem data,
+# building a JuMP model and solving it.
+
+function fit_ridge(model, X, y, α)
+    JuMP.empty!(model)
+    set_silent(model)
+    N, D = size(X)
+    @variable(model, w[1:D])
+    err_term = X * w - y
+    @objective(
+        model,
+        Min,
+        dot(err_term, err_term) / (2 * N * D) + α * dot(w, w) / (2 * D),
+    )
+    optimize!(model)
+    @assert termination_status(model) in [MOI.OPTIMAL, MOI.LOCALLY_SOLVED, MOI.ALMOST_LOCALLY_SOLVED]
+    return w
+end
+
+# We can solve the problem for several values of α
+# to visualize the effect of regularization on the testing and training loss.
+
+αs = 0.00:0.01:0.50
+mse_test = Float64[]
+mse_train = Float64[]
+model = Model(() -> DiffOpt.diff_optimizer(Ipopt.Optimizer))
+(Ntest, D) = size(X_test)
+(Ntrain, D) = size(X_train)
+for α in αs
+    w = fit_ridge(model, X_train, y_train, α)
+    ŵ = value.(w)
+    ŷ_test = X_test * ŵ 
+    ŷ_train = X_train * ŵ 
+    push!(mse_test, norm(ŷ_test - y_test)^2 / (2 * Ntest * D))
+    push!(mse_train, norm(ŷ_train - y_train)^2 / (2 * Ntrain * D))
+end
+
+# Visualize the Mean Score Error metric
+
+Plots.plot(
+    αs, mse_test ./ sum(mse_test),
+    label="MSE test", xaxis = "α", yaxis="MSE", legend=(0.8, 0.2),
+    width=3,
+)
+Plots.plot!(
+    αs, mse_train ./ sum(mse_train),
+    label="MSE train",
+    linestyle=:dash,
+    width=3,
+)
+Plots.title!("Normalized MSE on training and testing sets")
+
+# ## Leveraging differentiable optimization: computing the derivative of the solution
+
+# Using DiffOpt, we can compute `∂w_i/∂α`, the derivative of the learned solution `̂w`
+# w.r.t. the regularization parameter.
+
+function compute_dw_dα(model, w)
+    D = length(w)
+    dw_dα = zeros(D)
+    MOI.set(
+        model, 
+        DiffOpt.ForwardObjectiveFunction(),
+        dot(w, w)  / (2 * D),
+    )
+    DiffOpt.forward_differentiate!(model)
+    for i in 1:D
+        dw_dα[i] = MOI.get(
+            model,
+            DiffOpt.ForwardVariablePrimal(), 
+            w[i],
+        )
+    end
+    return dw_dα
+end
+
+# Using `∂w_i/∂α` computed with `compute_dw_dα`,
+# we can compute the derivative of the test loss w.r.t. the parameter α
+# by composing derivatives.
+
+function d_testloss_dα(model, X_test, y_test, w, ŵ)
+    N, D = size(X_test)
+    dw_dα = compute_dw_dα(model, w)
+    err_term = X_test * ŵ - y_test
+    return sum(eachindex(err_term)) do i
+        dot(X_test[i,:], dw_dα) * err_term[i]
+    end / (N * D)
+end
+
+# We can define a meta-optimizer function performing gradient descent
+# on the test loss w.r.t. the regularization parameter.
+
+function descent(α0, max_iters=100; fixed_step = 0.01, grad_tol=1e-3)
+    α_s = Float64[]
+    ∂α_s = Float64[]
+    test_loss = Float64[]
+    α = α0
+    N, D = size(X_test)
+    model = Model(() -> DiffOpt.diff_optimizer(Ipopt.Optimizer))
+    for iter in 1:max_iters
+        w = fit_ridge(model, X_train, y_train, α)
+        ŵ = value.(w)
+        err_term = X_test * ŵ - y_test
+        ∂α = d_testloss_dα(model, X_test, y_test, w, ŵ)
+        push!(α_s, α)
+        push!(∂α_s, ∂α)
+        push!(test_loss, norm(err_term)^2 / (2 * N * D))
+        α -= fixed_step * ∂α
+        if abs(∂α) ≤ grad_tol
+            break
+        end
+    end
+    return α_s, ∂α_s, test_loss
+end
+
+ᾱ, ∂ᾱ, msē = descent(0.10, 500)
+iters = 1:length(ᾱ);
+
+# Visualize gradient descent and convergence
+
+Plots.plot(
+    αs, mse_test,
+    label="MSE test", xaxis = ("α"),
+    legend=:topleft,
+    width=2,
+)
+Plots.plot!(
+    ᾱ, msē,
+    label="learned α", width = 5,
+    style=:dot,
+)
+Plots.title!("Regularizer learning")
+
+# Visualize the convergence of α to its optimal value
+
+Plots.plot(
+    iters, ᾱ, label = nothing, color = :blue,
+    xaxis = ("Iterations"), legend=:bottom,
+    title = "Convergence of α"
+)
+
+# Visualize the convergence of the objective function
+
+Plots.plot(
+    iters, msē, label = nothing, color = :red,
+    xaxis = ("Iterations"), legend=:bottom,
+    title = "Convergence of MSE"
+)
+
+# Visualize the convergence of the derivative to zero
+
+Plots.plot(
+    iters, ∂ᾱ, label = nothing, color = :green,
+    xaxis = ("Iterations"), legend=:bottom,
+    title = "Convergence of ∂α"
+)
---
+++ previews/PR225/examples/sensitivity-analysis-svm.jl
@@ -0,0 +1,130 @@
+# # Sensitivity Analysis of SVM
+
+#md # [![](https://img.shields.io/badge/show-github-579ACA.svg)](@__REPO_ROOT_URL__/docs/src/examples/sensitivity-analysis-svm.jl)
+
+# This notebook illustrates sensitivity analysis of data points in a [Support Vector Machine](https://en.wikipedia.org/wiki/Support-vector_machine) (inspired from [@matbesancon](http://github.com/matbesancon)'s [SimpleSVMs](http://github.com/matbesancon/SimpleSVMs.jl).)
+
+# For reference, Section 10.1 of https://online.stat.psu.edu/stat508/book/export/html/792 gives an intuitive explanation of what it means to have a sensitive hyperplane or data point. The general form of the SVM training problem is given below (with $\ell_2$ regularization):
+
+# ```math
+# \begin{split}
+# \begin{array} {ll}
+# \mbox{minimize} & \lambda||w||^2 + \sum_{i=1}^{N} \xi_{i} \\
+# \mbox{s.t.} & \xi_{i} \ge 0 \quad \quad i=1..N  \\
+#             & y_{i} (w^T X_{i} + b) \ge 1 - \xi_{i} \quad i=1..N  \\
+# \end{array}
+# \end{split}
+# ```
+# where
+# - `X`, `y` are the `N` data points
+# - `w` is the support vector
+# - `b` determines the offset `b/||w||` of the hyperplane with normal `w`
+# - `ξ` is the soft-margin loss
+# - `λ` is the $\ell_2$ regularization.
+#
+# This tutorial uses the following packages
+
+using JuMP     # The mathematical programming modelling language
+import DiffOpt # JuMP extension for differentiable optimization
+import Ipopt   # Optimization solver that handles quadratic programs
+import Plots   # Graphing tool
+import LinearAlgebra: dot, norm
+import Random
+
+# ## Define and solve the SVM
+
+# Construct two clusters of data points.
+
+N = 100
+D = 2
+
+Random.seed!(62)
+X = vcat(randn(N ÷ 2, D), randn(N ÷ 2, D) .+ [2.0, 2.0]')
+y = append!(ones(N ÷ 2), -ones(N ÷ 2))
+λ = 0.05;
+
+
+# Let's initialize a special model that can understand sensitivities
+
+model = Model(() -> DiffOpt.diff_optimizer(Ipopt.Optimizer))
+MOI.set(model, MOI.Silent(), true)
+
+# Add the variables
+
+@variable(model, ξ[1:N] >= 0)
+@variable(model, w[1:D])
+@variable(model, b);
+
+# Add the constraints.
+
+@constraint(model, con[i in 1:N],
+    y[i] * (dot(X[i, :], w) + b) >= 1 - ξ[i]
+);
+
+
+# Define the objective and solve
+
+@objective(model,
+    Min, λ * dot(w, w) + sum(ξ),
+)
+
+optimize!(model)
+
+
+# We can visualize the separating hyperplane.
+
+loss = objective_value(model)
+
+wv = value.(w)
+
+bv = value(b)
+
+svm_x = [-2.0, 4.0] # arbitrary points
+svm_y = (-bv .- wv[1] * svm_x )/wv[2]
+
+p = Plots.scatter(X[:,1], X[:,2], color = [yi > 0 ? :red : :blue for yi in y], label = "")
+Plots.plot!(p, svm_x, svm_y, label = "loss = $(round(loss, digits=2))", width=3)
+
+# ## Gradient of hyperplane wrt the data point coordinates
+
+# Now that we've solved the SVM, we can compute the sensitivity of optimal
+# values -- the separating hyperplane in our case -- with respect to
+# perturbations of the problem data -- the data points -- using DiffOpt.
+
+# How does a change in coordinates of the data points, `X`,
+# affects the position of the hyperplane?
+# This is achieved by finding gradients of `w` and `b` with respect to `X[i]`.
+
+# Begin differentiating the model.
+# analogous to varying θ in the expression:
+# ```math
+# y_{i} (w^T (X_{i} + \theta) + b) \ge 1 - \xi_{i}
+# ```
+∇ = zeros(N)
+for i in 1:N
+    for j in 1:N
+        if i == j
+            ## we consider identical perturbations on all x_i coordinates
+            MOI.set(model, DiffOpt.ForwardConstraintFunction(), con[j], y[j] * sum(w))
+        else
+            MOI.set(model, DiffOpt.ForwardConstraintFunction(), con[j], 0.0)
+        end
+    end
+    DiffOpt.forward_differentiate!(model)
+    dw = MOI.get.(model, DiffOpt.ForwardVariablePrimal(), w)
+    db = MOI.get(model, DiffOpt.ForwardVariablePrimal(), b)
+    ∇[i] = norm(dw) + norm(db)
+end
+
+# We can visualize the separating hyperplane sensitivity with respect to the data points.
+# Note that all the small numbers were converted into 1/10 of the
+# largest value to show all the points of the set.
+
+p3 = Plots.scatter(
+    X[:,1], X[:,2],
+    color = [yi > 0 ? :red : :blue for yi in y], label = "",
+    markersize = 2 * (max.(1.8∇, 0.2 * maximum(∇))),
+)
+Plots.yaxis!(p3, (-2, 4.5))
+Plots.plot!(p3, svm_x, svm_y, label = "", width=3)
+Plots.title!("Sensitivity of the separator to data point variations")
