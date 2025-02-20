200299d952d895260dd09fe6bb4a3ff1f55e3b8f
Andrew <arosemberg3@gatech.edu>
2025-02-20 12:31:33 -0500

    add test get ReverseConstraintDual

--- test/nlp_program.jl
+++ test/nlp_program.jl
@@ -727,6 +727,14 @@ function test_ReverseConstraintDual()
     Δλ = [0.1 for _ in 1:2]
     MOI.set.(m, DiffOpt.ReverseConstraintDual(), con, Δλ)
 
+    # test get ReverseConstraintDual
+    @test all(
+        [
+            MOI.get(m, DiffOpt.ReverseConstraintDual(), con[i]) == Δλ[i] for
+            i in 1:2
+        ]
+    )
+
     # Compute derivatives
     DiffOpt.reverse_differentiate!(m)
 
