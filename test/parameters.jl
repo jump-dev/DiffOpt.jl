2bba1b479354fbc1a3551cc2508ee943d5e2f2f0
Andrew <arosemberg3@gatech.edu>
2025-08-19 16:12:29 -0400

    integrate new POI

--- test/parameters.jl
+++ test/parameters.jl
@@ -663,6 +663,12 @@ function test_diff_errors()
         DiffOpt.ReverseConstraintFunction(),
         cons,
     )
+    @test_throws ErrorException MOI.set(
+        model,
+        DiffOpt.ForwardConstraintFunction(),
+        cons,
+        constraint_equation = convert(MOI.ScalarAffineFunction{Float64}, 1.0)
+    )
     return
 end
 
